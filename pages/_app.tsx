4e7683037fa50353198240ff7b51684895796cf5
Kitravee Siwatkittisuk <50334192+kitravee@users.noreply.github.com>
2021-07-19 04:06:44 +0700

    Revert "Materialui setup"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { SettingsProvider } from 'src/contexts/SettingsContext';
-import MuiThemeWrapper from '@/theme/MuiThemeWrapper';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SettingsProvider>
-      <MuiThemeWrapper>
-        <Component {...pageProps} />
-      </MuiThemeWrapper>
-    </SettingsProvider>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
--- src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const WE_ARE_FRIEND_VERSION = '0.0.1';
-
-export const THEMES = {
-  LIGHT: 'LIGHT',
-  ONE_DARK: 'ONE_DARK',
-  UNICORN: 'UNICORN',
-};
