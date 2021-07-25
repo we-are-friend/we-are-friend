140d56750abeecac08d5c4605ca473136ee97159
kitravee <kitravee.swk@gmail.com>
2021-07-25 22:57:37 +0700

    Add - axios and axios-mock-adapter

---
+++ src/__mocks__/user.ts
@@ -0,0 +1,16 @@
+import mock from 'src/utils/axiosMock';
+
+type User = {
+  name: string;
+  address: string;
+  phone: string;
+};
+
+mock.onGet('/user').reply(() => {
+  const userObject: User = {
+    name: 'Trong',
+    address: 'Bangkok',
+    phone: '0944645351',
+  };
+  return [200, userObject];
+});
