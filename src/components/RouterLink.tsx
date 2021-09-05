103e62f1d8f36d266bd646f150fda9aa309306b4
kitravee <kitravee.swk@gmail.com>
2021-09-05 22:50:29 +0700

    component - routerlink is material ui + nextlink

---
+++ src/components/RouterLink.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef } from 'react';
+import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+import MuiLink, { LinkProps as MuiLinkProps } from '@material-ui/core/Link';
+import { UrlObject } from 'url';
+// import { useTranslation } from 'next-i18next';
+
+interface RouterLinkProps {
+  className?: string;
+  children?: any;
+  hrefAs?: string | UrlObject | undefined;
+}
+
+export type NextLinkComposedProps = Omit<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  'href'
+> &
+  Omit<NextLinkProps, 'href'> & {
+    to: NextLinkProps['href'];
+    href?: any;
+  } & RouterLinkProps &
+  MuiLinkProps;
+
+// eslint-disable-next-line react/display-name
+const RouterLink = forwardRef<HTMLAnchorElement, NextLinkComposedProps>(
+  (props, ref) => {
+    const { to, as, replace, scroll, shallow, prefetch, children, ...other } =
+      props;
+    // const { i18n } = useTranslation();
+    return (
+      <NextLink
+        passHref
+        as={as}
+        href={to}
+        // locale={i18n.language}
+        prefetch={prefetch}
+        replace={replace}
+        scroll={scroll}
+        shallow={shallow}
+      >
+        <MuiLink ref={ref} underline="none" {...other}>
+          {children}
+        </MuiLink>
+      </NextLink>
+    );
+  },
+);
+
+export default RouterLink;
